170527cdd6b7bc172004e863e56b706c7e0798f5
Irina Kats <irina8kats@gmail.com>
2023-07-01 12:29:35 -0400

    step 9 done

---
+++ 09 Intro-to-Vue-3/main.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            cart: [],
+            premium: true
+        }
+    },
+    methods: {
+        incrementCart(id) {
+            this.cart.push(id)
+        },
+        decrementCart(id) {
+            let index =  this.cart.indexOf(id)
+            if (index !== -1) {
+                this.cart.splice(index, 1);
+              }
+        }
+    }
+})
